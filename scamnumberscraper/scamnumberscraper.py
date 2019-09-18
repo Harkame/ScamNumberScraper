7048145cb566ec28a843a961f3f17ea254372cd2
Harkame <louisr.daviaud@gmail.com>
2019-09-18 13:50:06 +0200

    more providers

--- scamnumberscraper/scamnumberscraper.py
+++ scamnumberscraper/scamnumberscraper.py
@@ -4,6 +4,12 @@ import requests
 from bs4 import BeautifulSoup
 
 DIXHUIT_URL = "http://www.dixhuit.fr"
+SIGNAL_ARNAQUES_URL = (
+    "https://www.signal-arnaques.com/phone-fraud?ajax=scam-table&Scam_page="
+)
+ARNAQUES_INTERNET_URL = (
+    "http://www.arnaques-internet.info/modules.php?name=telephone&pagenum="
+)
 
 
 def dixhuit(page):
@@ -40,5 +46,41 @@ def dixhuit_page(page):
         print(card_title_tag.text)
 
 
+def signal_arnaques():
+    pass
+
+
+def signal_arnaques_page(page):
+    response = requests.get(f"{SIGNAL_ARNAQUES_URL}{page}")
+
+    page = BeautifulSoup(response.content, features="lxml")
+
+    td_tags = page.find("tbody").find_all("td")
+
+    cleared_tags = []
+
+    for i in range(1, len(td_tags), 4):
+        cleared_tags.append(td_tags[i])
+
+    for tag in cleared_tags:
+        print(tag.text)
+
+
+def arnaques_internet(page):
+    response = requests.get(f"{ARNAQUES_INTERNET_URL}{page}")
+
+    page = BeautifulSoup(response.content, features="lxml")
+
+    table = page.find("table", {"class": "table"})
+
+    tr_tags = table.find_all("tr")
+
+    for i in range(1, len(tr_tags)):
+        b = tr_tags[i].find_all("td")[0].find("b")
+        print(b.text)
+
+
 if __name__ == "__main__":
-    dixhuit(1)
+    # dixhuit(1)
+    # signal_arnaques_page(3)
+    arnaques_internet(88)
